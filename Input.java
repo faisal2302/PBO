60a60d40e994784f61fac7cd810f7d6b0bbe54df
faisal <145694372+faisal2302@users.noreply.github.com>
2026-04-08 00:07:18 +0700

    Delete Input.java

--- Input.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.JOptionPane;
-
-public class Input {
-    public static void main(String[] args) {
-        String pelajaran = JOptionPane.showInputDialog("Anda sedang belajar apa?");
-        JOptionPane.showMessageDialog(null,
-            "Belajar " + pelajaran + " sangat mudah");
-    }
-}
